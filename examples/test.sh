a2b8cefc22e695698d06037faa3c35766e4574e2
JosephMillsAtWork <josephjamesmills@gmail.com>
2017-12-05 10:51:57 -0500

    fix up the proto file to expose models and also contianers

--- examples/test.sh
+++ examples/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#        --plugin=protoc-gen-qobject_macro_plugin=$(pwd)/QObjectMacroPlugin \
 /usr/local/bin/protoc \
         --plugin=protoc-gen-qprotocol_buffers_plugin=$(pwd)/QProtocolBuffers \
-        --qobject_macro_plugin_out=$(pwd)/qobj \
+        --qobjectbuffers_plugin_out=$(pwd)/qobj \
 	--proto_path=$(pwd) \
-	$(pwd)/BushingEvent.proto 
-
+	$(pwd)/Example.proto
 
 
